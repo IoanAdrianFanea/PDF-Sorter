0b90bafe52b78c4b400cc61fffb7379b5f757538
IoanAdrianFanea <ec24483@qmul.ac.uk>
2026-05-02 09:37:21 +0100

    chore: clean file directory and remove useless files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./prisma/prisma.module";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DocumentsModule } from './documents/documents.module';
-import { TagsModule } from './tags/tags.module';
 import { ExportsModule } from './exports/exports.module';
 import path from "path";
 
@@ -20,10 +17,7 @@ import path from "path";
     AuthModule,
     UsersModule,
     DocumentsModule,
-    TagsModule,
     ExportsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
